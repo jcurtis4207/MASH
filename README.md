37f94e8d67c243b4e1c792c8394a9ea31a4318ac
Jacob Curtis <jcurtis4207@gmail.com>
2021-09-09 15:58:06 -0400

    Add Error Handling and Multiple Command Execution
    This commit focuses on multiple command execution. To achieve this, the
    commands are stored in a structure with associated operator and error
    level. The error checking currently detects a single & as a bad
    operator, or unknown commands. The error level combined with the
    operators ;, &&, and || determine which commands are executed.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ MASH is a simple linux-based shell written in C++.
 
 It has a prompt showing the current user, hostname, and directory (with a colorscheme that matches my prompt).
 
-It only accepts single commands, no pipes, no semicolons, and no chaining.
+It can accept multiple commands using semicolons, or conditional execution using && and ||.
 
 If the input is not a built-in function, the input is executed with no error checking.
 
@@ -14,15 +14,13 @@ If the input is not a built-in function, the input is executed with no error che
 The current built-in functions are:
 * cd
 * clear
-* fetch (displaying an ASCII art rendition of M*A*S*H)
+* fetch (displaying an ASCII art rendition of MASH)
 * exit
 
 # 
 
 ### To-do
 These are the features that will eventually be implemented:
-* error checking and error level passing
-* multiple commands via semicolons, &&, and ||
 * piping
 * expanding '~' to the user's home directory
 * dollar sign variables
--- mash.cpp
+++ mash.cpp
@@ -4,6 +4,8 @@
 */
 
 #include <iostream>
+#include <map>
+#include <regex>
 #include <sstream>
 #include <string.h>
 #include <sys/wait.h>
@@ -12,10 +14,31 @@
 
 using namespace std;
 
+#define SEMICOLON 0
+#define DOUBLE_AND 1
+#define DOUBLE_OR 2
+#define PIPE 3
+
+struct Command
+{
+    string program;
+    vector<string> stringArgs;
+    int op = 0;
+    int error = 0;
+    Command(const string prog, vector<string> args, const int oper)
+        : program(prog), stringArgs(args), op(oper){}
+    Command(const int el)
+        : error(el){}
+};
+
 string username;
 string hostname;
 string currentDir;
-bool update;
+bool updatePrompt;
+const map<int, string> errors = {
+    {1, "Unknown Operator"}, 
+    {2, "Unknown Command"}
+};
 
 void clearScreen()
 {
@@ -41,12 +64,12 @@ void setGlobalParameters()
     username = getlogin();
     hostname = hName;
     currentDir = cDir;
-    update = false;
+    updatePrompt = false;
 }
 
 void showPrompt()
 {
-    if(update)
+    if(updatePrompt)
         setGlobalParameters();
     stringstream prompt;
     prompt << "\033[1;31m" << "M*A*S*H "; // red
@@ -58,15 +81,65 @@ void showPrompt()
     cout << prompt.str();
 }
 
+void throwError(const string& message)
+{
+    cout << "ERROR: " << message << "\n";
+}
+
+void getCommands(vector<Command>& commands, string input)
+{
+    stringstream builder;
+    for(unsigned long i = 0; i < input.length(); i++)
+    {
+        if(input[i] == ';')
+        {
+            commands.push_back(Command(builder.str(), vector<string>(), SEMICOLON));
+            stringstream().swap(builder);
+        }
+        else if(input[i] == '&')
+        {
+            if(input[i + 1] == '&')
+            {
+                commands.push_back(Command(builder.str(), vector<string>(), DOUBLE_AND));
+                i++;
+                stringstream().swap(builder);
+            }
+            else
+            {
+                commands.push_back(Command(1));
+                stringstream().swap(builder);
+            }
+        }
+        else if(input[i] == '|')
+        {
+            if(input[i + 1] == '|')
+            {
+                commands.push_back(Command(builder.str(), vector<string>{}, DOUBLE_OR));
+                i++;
+            stringstream().swap(builder);
+            }
+            else
+            {
+                commands.push_back(Command(builder.str(), vector<string>(), PIPE));
+                stringstream().swap(builder);
+            }
+        }
+        else
+            builder << input[i];
+    }
+    commands.push_back(Command(builder.str(), vector<string>(), SEMICOLON));
+}
+
 string getProgram(string input)
 {
+    input = regex_replace(input, regex("^ +"), "");
     return input.substr(0, input.find(" "));
 }
 
 vector<string> getArgs(string input)
 {
     vector<string> args;
-    stringstream inputStream(input);
+    stringstream inputStream(regex_replace(input, regex("^ +"), ""));
     string temp;    
     while (getline(inputStream, temp, ' '))
         args.push_back(temp);
@@ -80,7 +153,7 @@ bool executeBuiltins(const string& program, char* arg1)
     else if(program == "cd")
     {
         chdir(arg1);
-        update = true;
+        updatePrompt = true;
         return false;
     }
     else if(program == "clear")
@@ -96,47 +169,77 @@ bool executeBuiltins(const string& program, char* arg1)
     else
         return true;
 
-    // su - update = true
+    // su - updatePrompt = true
+}
+
+void executeCommand(Command& command)
+{
+    const vector<string> stringArgs = getArgs(command.program);
+    const string program = getProgram(command.program);
+    if(command.error != 0)
+    {
+        throwError(errors.at(command.error));
+        return;
+    }
+    // convert strings to char*
+    const unsigned long argSize = stringArgs.size();
+    const char* cmd = program.c_str();
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wvla-extension"
+    char* args[argSize + 1];
+#pragma GCC diagnostic pop
+    for(unsigned long i = 0; i < argSize; i++)
+    {
+        args[i] = new char[stringArgs[i].size() + 1];
+        strcpy(args[i], stringArgs[i].c_str());
+    }
+    args[argSize] = NULL;
+    // execute command
+    if(executeBuiltins(program, args[1]))
+    {
+        if(fork() != 0)
+            wait(NULL);
+        else
+        {
+            if(execvp(cmd, args) != 0)
+            {
+                command.error = 2;
+                throwError(errors.at(command.error));
+            }
+        }
+    }
+    // clean up pointers
+    for(unsigned long i = 0; i < argSize; i++)
+        delete [] args[i];
 }
 
 int main()
 {
+    vector<Command> commands;
     clearScreen();
     printFetch();
-    update = true;
+    updatePrompt = true;
+    int errorLevel = 0;
+    int currentOperator = SEMICOLON;
     while(true)
     {
         showPrompt();
-        // read command
         string line;
         getline(cin, line);
-        string program = getProgram(line);
-        vector<string> stringArgs = getArgs(line);
-        // convert strings to char arrays
-        const unsigned long argSize = stringArgs.size();
-        const char* cmd = program.c_str();
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wvla-extension"
-        char* args[argSize + 1];
-#pragma GCC diagnostic pop
-        for(unsigned long i = 0; i < argSize; i++)
+        getCommands(commands, line);
+        for(auto& command : commands)
         {
-            args[i] = new char[stringArgs[i].size() + 1];
-            strcpy(args[i], stringArgs[i].c_str());
+            // operator and error level determines execution
+            if(currentOperator == SEMICOLON)
+                executeCommand(command);
+            else if(currentOperator == DOUBLE_OR && errorLevel != 0)
+                executeCommand(command);
+            else if(currentOperator == DOUBLE_AND && errorLevel == 0)
+                executeCommand(command);
+            currentOperator = command.op;
+            errorLevel = command.error;
         }
-        args[argSize] = NULL;
-        // execute command
-        if(executeBuiltins(program, args[1]))
-        {
-            if(fork() != 0)
-                wait(NULL);
-            else
-                execvp(cmd, args);
-        }
-        // clean up
-        for(unsigned long i = 0; i < argSize; i++)
-            delete [] args[i];
-            
+      commands.clear();
     }
-    return 0;
+    return errorLevel;
 }

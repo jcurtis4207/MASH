0bb37a2d264eef09c7172a310f328c75b7e92275
Jacob Curtis <jcurtis4207@gmail.com>
2021-09-12 23:21:19 -0400

    Rename Files

---
+++ Aliases.cpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+
+// find file
+// return silent error if not opened
+
+// create vector of aliases
+// ignore comments
+
+// in execution, new function before builtins: test input against aliases
+// after getProgram, before getCharPtrArray
+// if found, add args to start of stringArgs
---
+++ Main.cpp
@@ -0,0 +1,77 @@
+/*
+* clang++ -std=c++17 -O2 mash.cpp -o mash 
+*   with added warnings
+*/
+
+#include <iostream>
+#include <vector>
+#include "Math.cpp"
+#include "Parser.cpp"
+#include "Execution.cpp"
+
+using namespace std;
+
+extern bool updatePrompt;
+
+int main()
+{
+    if(setvbuf(stdout, NULL, _IONBF, 0) != 0)
+    {
+        cerr << "Stdout failed to set as unbuffered\n";
+        exit(1);
+    }
+    seteuid(getuid());
+    clearScreen();
+    printFetch();
+    updatePrompt = true;
+    int errorLevel = 0;
+    int currentOperator = SEMICOLON;
+    vector<Command> commands;
+    while(true)
+    {
+        showPrompt();
+        string line = getInput();
+        pushHistory(line);
+        if(getCommands(commands, line) == 1)
+        {
+            mathMode(line.substr(4));
+            continue;
+        }
+        Command pipeStart = {"", SEMICOLON};
+        bool piping = false;
+        for(auto& command : commands)
+        {
+            if(piping)
+            {
+                if(command.op == PIPE)
+                {
+                    throwError(3);
+                    break;
+                }
+                else
+                {
+                    executePipe(pipeStart, command);
+                    piping = false;
+                }
+            }
+            else
+            {
+                if(command.op == PIPE)
+                {
+                    pipeStart = command;
+                    piping = true;
+                    continue;
+                }
+                if(currentOperator == SEMICOLON)
+                    errorLevel = executeCommand(command, false);
+                else if(currentOperator == DOUBLE_OR && errorLevel != 0)
+                    errorLevel = executeCommand(command, false);
+                else if(currentOperator == DOUBLE_AND && errorLevel == 0)
+                    errorLevel = executeCommand(command, false);
+            }
+            currentOperator = command.op;
+        }
+        commands.clear();
+    }
+    return errorLevel;
+}
